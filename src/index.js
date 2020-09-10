8c8b7b030b7bac49c9c6ac3ceeaa56388860590a
Maria <parfenenkoma@gmail.com>
2020-09-10 18:36:42 +0300

    add styles for project

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
-// import React from "react"
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/app"
 
-class SwapiService {
-  _apiBase = "https://swapi.dev/api"
-
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`)
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, received ${res.status}`)
-    }
-    return await res.json()
-  }
-
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`)
-    return res.results
-  }
-
-  getPerson(id) {
-    return this.getResource(`/people/${id}`)
-  }
-}
-
-const swapi = new SwapiService()
-
-swapi.getPerson(3).then((person) => {
-  console.log(person.name)
-})
+ReactDOM.render(<App />, document.getElementById("root"))
